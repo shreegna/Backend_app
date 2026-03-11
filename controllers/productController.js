203e4f191d32a9d6fd4bbd8ebaf3b1c35f94e593
shreegna <shreegnakomire21@gmail.com>
2026-03-11 14:02:12 +0530

    commit

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,6 +3,7 @@ import productModel from "../models/productModel.js";
 const getProducts = async (req, res) => {
   const products = await productModel.find();
   res.render("products/index", { products });
+  //res.json(products);
 };
 
 const addProduct = async (req, res) => {
